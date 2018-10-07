edaf009a839e175970ee4d3704437aa99dd77ef3
Chris de Vreeze <chris.de.vreeze@caiway.net>
2018-10-07 22:21:04 +0200

    More tests in FormulaSimulationTest

--- jvm/src/test/scala/eu/cdevreeze/xbrl/formula/xmlmapping/FormulaSimulationTest.scala
+++ jvm/src/test/scala/eu/cdevreeze/xbrl/formula/xmlmapping/FormulaSimulationTest.scala
@@ -50,6 +50,220 @@ class FormulaSimulationTest extends FunSuite {
 
   private val Iso4217Namespace = "http://www.xbrl.org/2003/iso4217"
 
+  test("test-12010-v-01") {
+    val parentDir = URI.create("conformance-formula-2018-09-13/10000%20Formula/12010-Formula-Processing-ConstructingFactValue/")
+    val parentDirUri = dummyUriPrefix.resolve(parentDir)
+    val instanceUri: URI = parentDirUri.resolve("12010-just-a-number-instance.xml")
+
+    val xbrlInstance: XbrlInstance =
+      XbrlInstanceDocument.build(docBuilder.build(instanceUri)).documentElement
+
+    val taxo =
+      makeTestDts(
+        xbrlInstance.findAllSchemaRefs.map(_.resolvedHref).toSet
+          .union(xbrlInstance.findAllLinkbaseRefs.map(_.resolvedHref).toSet))
+
+    val oimMapper = new XmlToOimMapper(taxo)
+
+    val report = oimMapper.convertXbrlInstance(xbrlInstance)
+
+    val ns = "http://xbrl.org/formula/conformance/example"
+
+    // Simulate formula in 12010-just-a-number-formula.xml
+
+    // Use (fast) concept name filters first.
+    // Now we probably have a small sequence of fact values for these fact variables to process.
+
+    val n1Facts = report.findNumericSimpleFactsByName(EName(ns, "n1"))
+
+    val resultFacts: immutable.IndexedSeq[NumericSimpleFact] =
+      for {
+        n1Fact <- n1Facts
+      } yield {
+        NumericSimpleFact.from(
+          None,
+          n1Fact.aspectValueSet,
+          Some(NumericValue(BigDecimal(1.2))),
+          FiniteAccuracy(1))
+      }
+
+    val expectedResultFacts =
+      List(
+        NumericSimpleFact.from(
+          None,
+          AspectValueSet.Empty
+            .withConcept(EName(ns, "n1"))
+            .withEntity(URI.create("http://xbrl.org/entity/identification/scheme"), "01")
+            .withPeriod(LocalTimeInterval.fromLocalDate(LocalDate.of(2007, 12, 31)))
+            .withUnit(Set(EName(Namespaces.XbrliNamespace, "pure"))),
+          Some(NumericValue(BigDecimal(1.2))),
+          FiniteAccuracy(1)))
+
+    assertResult(expectedResultFacts) {
+      resultFacts
+    }
+  }
+
+  test("test-12010-v-08") {
+    val parentDir = URI.create("conformance-formula-2018-09-13/10000%20Formula/12010-Formula-Processing-ConstructingFactValue/")
+    val parentDirUri = dummyUriPrefix.resolve(parentDir)
+    val instanceUri: URI = parentDirUri.resolve("12010-empty-intersection-instance.xml")
+
+    val xbrlInstance: XbrlInstance =
+      XbrlInstanceDocument.build(docBuilder.build(instanceUri)).documentElement
+
+    val taxo =
+      makeTestDts(
+        xbrlInstance.findAllSchemaRefs.map(_.resolvedHref).toSet
+          .union(xbrlInstance.findAllLinkbaseRefs.map(_.resolvedHref).toSet))
+
+    val oimMapper = new XmlToOimMapper(taxo)
+
+    val report = oimMapper.convertXbrlInstance(xbrlInstance)
+
+    val aspectUniverse = report.aspectUniverse
+
+    val ns = "http://xbrl.org/formula/conformance/example"
+
+    // Simulate formula in 12010-intersection-formula.xml
+
+    val uncoveredAspects =
+      aspectUniverse.filterNot(_.isInstanceOf[DimensionAspect]).diff(Set(ConceptAspect, PeriodAspect))
+
+    // Use (fast) concept name filter for factVar1 etc., and use the knowledge that it matches only numeric simple facts.
+    // Now we probably have a small sequence of fact value sequences for this fact variable to process.
+
+    val n1FactSeqs: Map[AspectValueSet, immutable.IndexedSeq[NumericSimpleFact]] =
+      report.findNumericSimpleFactsByName(EName(ns, "n1"))
+        .groupBy(_.aspectValueSet.filteringAspects(uncoveredAspects))
+
+    val n2FactSeqs: Map[AspectValueSet, immutable.IndexedSeq[NumericSimpleFact]] =
+      report.findNumericSimpleFactsByName(EName(ns, "n2"))
+        .groupBy(_.aspectValueSet.filteringAspects(uncoveredAspects))
+
+    val n3Facts = report.findNumericSimpleFactsByName(EName(ns, "n3"))
+
+    val resultFacts: immutable.IndexedSeq[NumericSimpleFact] =
+      for {
+        (aspectValueSet1, n1FactSeq) <- n1FactSeqs.toIndexedSeq
+        (aspectValueSet2, n2FactSeq) <- n2FactSeqs.toIndexedSeq
+        if aspectValueSet1.filteringAspects(uncoveredAspects) ==
+          aspectValueSet2.filteringAspects(uncoveredAspects)
+
+        n3Fact <- n3Facts
+        if aspectValueSet1.filteringAspects(uncoveredAspects) ==
+          n3Fact.aspectValueSet.filteringAspects(uncoveredAspects)
+      } yield {
+        val overlappingValues =
+          n1FactSeq.flatMap(_.factValueOption).toSet.intersect(n2FactSeq.flatMap(_.factValueOption).toSet)
+            .map(_.asBigDecimal)
+        val resultValueOption: Option[BigDecimal] =
+          if (overlappingValues.size == 1) Some(overlappingValues.head) else None
+
+        NumericSimpleFact.from(
+          None,
+          n3Fact.aspectValueSet,
+          resultValueOption.map(v => NumericValue(v)),
+          FiniteAccuracy(0))
+      }
+
+    val expectedResultFacts =
+      List(
+        NumericSimpleFact.from(
+          None,
+          AspectValueSet.Empty
+            .withConcept(EName(ns, "n3"))
+            .withEntity(URI.create("http://xbrl.org/entity/identification/scheme"), "01")
+            .withPeriod(LocalTimeInterval.fromLocalDate(LocalDate.of(2007, 12, 31)))
+            .withUnit(Set(EName(Namespaces.XbrliNamespace, "pure"))),
+          None,
+          FiniteAccuracy(0)))
+
+    assertResult(expectedResultFacts) {
+      resultFacts
+    }
+  }
+
+  test("test-12010-v-09") {
+    val parentDir = URI.create("conformance-formula-2018-09-13/10000%20Formula/12010-Formula-Processing-ConstructingFactValue/")
+    val parentDirUri = dummyUriPrefix.resolve(parentDir)
+    val instanceUri: URI = parentDirUri.resolve("12010-an-intersection-instance.xml")
+
+    val xbrlInstance: XbrlInstance =
+      XbrlInstanceDocument.build(docBuilder.build(instanceUri)).documentElement
+
+    val taxo =
+      makeTestDts(
+        xbrlInstance.findAllSchemaRefs.map(_.resolvedHref).toSet
+          .union(xbrlInstance.findAllLinkbaseRefs.map(_.resolvedHref).toSet))
+
+    val oimMapper = new XmlToOimMapper(taxo)
+
+    val report = oimMapper.convertXbrlInstance(xbrlInstance)
+
+    val aspectUniverse = report.aspectUniverse
+
+    val ns = "http://xbrl.org/formula/conformance/example"
+
+    // Simulate formula in 12010-intersection-formula.xml, but this time producing no nil fact
+
+    val uncoveredAspects =
+      aspectUniverse.filterNot(_.isInstanceOf[DimensionAspect]).diff(Set(ConceptAspect, PeriodAspect))
+
+    // Use (fast) concept name filter for factVar1 etc., and use the knowledge that it matches only numeric simple facts.
+    // Now we probably have a small sequence of fact value sequences for this fact variable to process.
+
+    val n1FactSeqs: Map[AspectValueSet, immutable.IndexedSeq[NumericSimpleFact]] =
+      report.findNumericSimpleFactsByName(EName(ns, "n1"))
+        .groupBy(_.aspectValueSet.filteringAspects(uncoveredAspects))
+
+    val n2FactSeqs: Map[AspectValueSet, immutable.IndexedSeq[NumericSimpleFact]] =
+      report.findNumericSimpleFactsByName(EName(ns, "n2"))
+        .groupBy(_.aspectValueSet.filteringAspects(uncoveredAspects))
+
+    val n3Facts = report.findNumericSimpleFactsByName(EName(ns, "n3"))
+
+    val resultFacts: immutable.IndexedSeq[NumericSimpleFact] =
+      for {
+        (aspectValueSet1, n1FactSeq) <- n1FactSeqs.toIndexedSeq
+        (aspectValueSet2, n2FactSeq) <- n2FactSeqs.toIndexedSeq
+        if aspectValueSet1.filteringAspects(uncoveredAspects) ==
+          aspectValueSet2.filteringAspects(uncoveredAspects)
+
+        n3Fact <- n3Facts
+        if aspectValueSet1.filteringAspects(uncoveredAspects) ==
+          n3Fact.aspectValueSet.filteringAspects(uncoveredAspects)
+      } yield {
+        val overlappingValues =
+          n1FactSeq.flatMap(_.factValueOption).toSet.intersect(n2FactSeq.flatMap(_.factValueOption).toSet)
+            .map(_.asBigDecimal)
+        val resultValueOption: Option[BigDecimal] =
+          if (overlappingValues.size == 1) Some(overlappingValues.head) else None
+
+        NumericSimpleFact.from(
+          None,
+          n3Fact.aspectValueSet,
+          resultValueOption.map(v => NumericValue(v)),
+          FiniteAccuracy(0))
+      }
+
+    val expectedResultFacts =
+      List(
+        NumericSimpleFact.from(
+          None,
+          AspectValueSet.Empty
+            .withConcept(EName(ns, "n3"))
+            .withEntity(URI.create("http://xbrl.org/entity/identification/scheme"), "01")
+            .withPeriod(LocalTimeInterval.fromLocalDate(LocalDate.of(2007, 12, 31)))
+            .withUnit(Set(EName(Namespaces.XbrliNamespace, "pure"))),
+          Some(NumericValue(BigDecimal(13))),
+          FiniteAccuracy(0)))
+
+    assertResult(expectedResultFacts) {
+      resultFacts
+    }
+  }
+
   test("test-12010-v-12") {
     val parentDir = URI.create("conformance-formula-2018-09-13/10000%20Formula/12010-Formula-Processing-ConstructingFactValue/")
     val parentDirUri = dummyUriPrefix.resolve(parentDir)
@@ -154,14 +368,16 @@ class FormulaSimulationTest extends FunSuite {
     val resultFacts: immutable.IndexedSeq[NumericSimpleFact] =
       for {
         m1Fact <- m1Facts
-        m2Fact <- m2Facts
-        m3Fact <- m3Facts
-        m4Fact <- m4Facts
 
+        m2Fact <- m2Facts
         if m1Fact.aspectValueSet.filteringAspects(uncoveredAspects) ==
           m2Fact.aspectValueSet.filteringAspects(uncoveredAspects)
+
+        m3Fact <- m3Facts
         if m2Fact.aspectValueSet.filteringAspects(uncoveredAspects) ==
           m3Fact.aspectValueSet.filteringAspects(uncoveredAspects)
+
+        m4Fact <- m4Facts
         if m3Fact.aspectValueSet.filteringAspects(uncoveredAspects) ==
           m4Fact.aspectValueSet.filteringAspects(uncoveredAspects)
       } yield {
@@ -193,6 +409,50 @@ class FormulaSimulationTest extends FunSuite {
 
     assertResult(expectedResultFacts) {
       resultFacts
+    }
+  }
+
+  test("test-33210-v-08") {
+    val parentDir = URI.create("conformance-formula-2018-09-13/30000%20Assertions/33210-ValueAssertion-Processing/")
+    val parentDirUri = dummyUriPrefix.resolve(parentDir)
+    val instanceUri: URI = parentDirUri.resolve("33210-seq-dupTest-1-instance.xml")
+
+    val xbrlInstance: XbrlInstance =
+      XbrlInstanceDocument.build(docBuilder.build(instanceUri)).documentElement
+
+    val taxo =
+      makeTestDts(
+        xbrlInstance.findAllSchemaRefs.map(_.resolvedHref).toSet
+          .union(xbrlInstance.findAllLinkbaseRefs.map(_.resolvedHref).toSet))
+
+    val oimMapper = new XmlToOimMapper(taxo)
+
+    val report = oimMapper.convertXbrlInstance(xbrlInstance)
+
+    val aspectUniverse = report.aspectUniverse
+
+    // Simulate formula in 33210-seq-dupTest-1-formula.xml
+
+    val uncoveredAspects = aspectUniverse
+
+    val factSeqs: Map[AspectValueSet, immutable.IndexedSeq[SimpleFact]] =
+      report.findAllSimpleFacts
+        .groupBy(_.aspectValueSet.filteringAspects(uncoveredAspects))
+
+    val evalResults: immutable.IndexedSeq[Boolean] =
+      for {
+        (aspectValueSet, factSeq) <- factSeqs.toIndexedSeq
+        if factSeq.size > 1
+      } yield {
+        val allEqual = factSeq.flatMap(_.factValueOption).distinct.size == 1
+        allEqual
+      }
+
+    assertResult(6) {
+      evalResults.size
+    }
+    assertResult(1) {
+      evalResults.filter(Set(true)).size
     }
   }
 
